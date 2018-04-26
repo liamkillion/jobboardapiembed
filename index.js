3ec90532cf2b692fa4c23a683977be68ff4c144e
Liam Killion <liam.killion@gmail.com>
2018-04-26 17:52:14 -0400

    i mispelt html

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ fetch(url).then((resp=>resp.json())).then(data=>data['jobs']).then(jobs => {
     if (job['location']['name'] === 'New York'){
       let li = document.createElement('li')
       document.getElementById('new_york').appendChild(li)
-      li.innerHTML = (`<a href=./one.hmtl?gh_jid=${job['id']}>${job['title']}</a>`)
+      li.innerHTML = (`<a href=./one.html?gh_jid=${job['id']}>${job['title']}</a>`)
     }
     else if (job['location']['name'] === 'Ybor City'){
       let li = document.createElement('li')
       document.getElementById('ybor_city').appendChild(li)
-      li.innerHTML = (`<a href=./three.hmtl?gh_jid=${job['id']}>${job['title']}</a>`)
+      li.innerHTML = (`<a href=./three.html?gh_jid=${job['id']}>${job['title']}</a>`)
     } else {
       let li = document.createElement('li')
       document.getElementById('terre_haute').appendChild(li)
-      li.innerHTML = (`<a href=./two.hmtl?gh_jid=${job['id']}>${job['title']}</a>`)
+      li.innerHTML = (`<a href=./two.html?gh_jid=${job['id']}>${job['title']}</a>`)
     }
   })
 })
