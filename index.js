1b4d347f78d508eb45141e51bcae310d7ce8e85c
Liam Killion <liam.killion@gmail.com>
2018-04-26 16:45:51 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let url = "https://api.greenhouse.io/v1/boards/liamcosandbox/jobs"
+fetch(url).then((resp=>resp.json())).then(data=>data['jobs']).then(jobs => {
+  jobs.forEach(job => {
+    if (job['location']['name'] === 'New York'){
+      let li = document.createElement('li')
+      document.getElementById('new_york').appendChild(li)
+      li.innerHTML = (`<a href=${job['absolute_url']}>${job['title']}</a>`)
+    }
+    else if (job['location']['name'] === 'Ybor City'){
+      let li = document.createElement('li')
+      document.getElementById('ybor_city').appendChild(li)
+      li.innerHTML = (`<a href=${job['absolute_url']}>${job['title']}</a>`)
+    } else {
+      let li = document.createElement('li')
+      document.getElementById('terre_haute').appendChild(li)
+      li.innerHTML = (`<a href=${job['absolute_url']}>${job['title']}</a>`)
+    }
+  })
+})
